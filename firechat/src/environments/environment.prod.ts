6083b8e0c9bf71496d760c54d92de29d8b835d24
Alan Cordova <info.devalan@gmail.com>
2020-09-03 15:15:51 -0500

    Generate deploy files

--- firechat/src/environments/environment.prod.ts
+++ firechat/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDUAzl20uVAkNksMbeTUrpfCIj3UBseJtI',
+    authDomain: 'firechat-b8f29.firebaseapp.com',
+    databaseURL: 'https://firechat-b8f29.firebaseio.com',
+    projectId: 'firechat-b8f29',
+    storageBucket: 'firechat-b8f29.appspot.com',
+    messagingSenderId: '285919034593',
+  },
 };
